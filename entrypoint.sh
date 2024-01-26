7ce50a05cc6c0d5d85f0d293782119d5eb3beac3
JavaScriptlee <155420066+JavaScriptlee@users.noreply.github.com>
2024-01-26 18:16:55 +1200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="894df7ef-157c-4540-b58d-6703dcc1cb2b"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
